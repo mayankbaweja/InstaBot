fba4395d3fb4b36d4a6a48730139991ee25fc0e9
mayankbaweja <mayankbawejarow@gmail.com>
2017-07-07 16:58:49 +0530

    Targeted commenting on any post using hashtags

--- instaBot.py
+++ instaBot.py
@@ -4,29 +4,29 @@ ACCESS_TOKEN = "1595866987.0d51ae1.ecb2290bd5f74b1584894260fc8cfd64"
 BASE_URL= "https://api.instagram.com/v1/"
 
 
-
+# Function to show our own information
 def apni_jaankari():
     print "GET request url :" + ((BASE_URL + "users/self/?access_token=%s") % (ACCESS_TOKEN))
-    user_information = requests.get((BASE_URL + "users/self/?access_token=%s") % (ACCESS_TOKEN)).json()
-    if user_information["meta"]["code"] == 200:
-        if len(user_information["data"]):
-            print "Username: %s" % (user_information["data"]["username"])
-            print "Followers: %s" % (user_information["data"]["counts"]["followed_by"])
-            print "Following: %s" % (user_information["data"]["counts"]["follows"])
-            print "Posts: %s" % (user_information["data"]["counts"]["media"])
+    upyogkarta_jankaari = requests.get((BASE_URL + "users/self/?access_token=%s") % (ACCESS_TOKEN)).json()
+    if upyogkarta_jankaari["meta"]["code"] == 200:
+        if len(upyogkarta_jankaari["data"]):
+            print "Username: %s" % (upyogkarta_jankaari["data"]["username"])
+            print "Followers: %s" % (upyogkarta_jankaari["data"]["counts"]["followed_by"])
+            print "Following: %s" % (upyogkarta_jankaari["data"]["counts"]["follows"])
+            print "Posts: %s" % (upyogkarta_jankaari["data"]["counts"]["media"])
         else:
             print "USER DOES NOT EXIST"
     else:
         print "RECIEVED STATUS CODE IS NOT 200"
 
 
-
+# Function to search any user's id
 def get_user_id(instagram_username):
     print 'GET request url :' + (BASE_URL + 'users/search?q=%s&access_token=%s') % (instagram_username, ACCESS_TOKEN)
-    user_information = requests.get((BASE_URL + 'users/search?q=%s&access_token=%s') % (instagram_username, ACCESS_TOKEN)).json()
-    if user_information["meta"]["code"] == 200:
-        if len(user_information["data"]):
-            return user_information["data"][0]["id"]
+    upyogkarta_jankaari = requests.get((BASE_URL + 'users/search?q=%s&access_token=%s') % (instagram_username, ACCESS_TOKEN)).json()
+    if upyogkarta_jankaari["meta"]["code"] == 200:
+        if len(upyogkarta_jankaari["data"]):
+            return upyogkarta_jankaari["data"][0]["id"]
         else:
             return None
     else:
@@ -34,27 +34,27 @@ def get_user_id(instagram_username):
         exit()
 
 
-
+# Function to show the basic information of any user like username, followers, posts etc.
 def get_user_information(instagram_username):
     user_id = get_user_id(instagram_username)
     if user_id == None:
         print 'User does not exist!'
         exit()
     print 'GET request url :' + (BASE_URL + 'users/%s?access_token=%s') % (user_id, ACCESS_TOKEN)
-    user_information = requests.get((BASE_URL + 'users/%s?access_token=%s') % (user_id, ACCESS_TOKEN)).json()
-    if user_information["meta"]["code"] == 200:
-        if len(user_information["data"]):
-            print "Username: %s" % (user_information["data"]["username"])
-            print "Followers: %s" % (user_information["data"]["counts"]["followed_by"])
-            print "Following: %s" % (user_information["data"]["counts"]["follows"])
-            print "Posts: %s" % (user_information["data"]["counts"]["media"])
+    upyogkarta_jankaari = requests.get((BASE_URL + 'users/%s?access_token=%s') % (user_id, ACCESS_TOKEN)).json()
+    if upyogkarta_jankaari["meta"]["code"] == 200:
+        if len(upyogkarta_jankaari["data"]):
+            print "Username: %s" % (upyogkarta_jankaari["data"]["username"])
+            print "Followers: %s" % (upyogkarta_jankaari["data"]["counts"]["followed_by"])
+            print "Following: %s" % (upyogkarta_jankaari["data"]["counts"]["follows"])
+            print "Posts: %s" % (upyogkarta_jankaari["data"]["counts"]["media"])
         else:
             print "There is no data for this user"
     else:
         print "Recieved status code is not 200"
 
 
-
+# Function to show our own recent posts
 def khudki_post():
     print "GET request url :" + (BASE_URL + 'users/self/media/recent/?access_token=%s') % (ACCESS_TOKEN)
     apni_post = requests.get((BASE_URL + 'users/self/media/recent/?access_token=%s') % (ACCESS_TOKEN)).json()
@@ -71,7 +71,7 @@ def khudki_post():
         print "Recieved status code is not 200"
 
 
-
+# Function to get any post's id
 def get_post_id(instagram_username):
     user_id = get_user_id(instagram_username)
     if user_id == None:
@@ -90,7 +90,7 @@ def get_post_id(instagram_username):
         exit()
 
 
-
+# Function to download recent post of any user
 def get_users_post(instagram_username):
     user_id = get_user_id(instagram_username)
     if user_id == None:
@@ -112,14 +112,14 @@ def get_users_post(instagram_username):
     return user_media
 
 
-
+# Function to show the recent posts liked by the owner of Access token
 def liked_by_user():
     print "Get request URL:" + ((BASE_URL + "users/self/media/liked?access_token=%s") % (ACCESS_TOKEN))
     liked = requests.get((BASE_URL + "users/self/media/liked?access_token=%s") % (ACCESS_TOKEN)).json()
     print liked["data"][0]["id"]
 
 
-
+# Function to get the list of comments on any post
 def get_the_comments(instagram_username):
     post_id = get_post_id(instagram_username)
     print "Get request URL:" + ((BASE_URL + "media/%s/comments?access_token=%s") % (post_id, ACCESS_TOKEN))
@@ -127,8 +127,8 @@ def get_the_comments(instagram_username):
     print comments["data"]
 
 
-
-def like_a_post(instagram_username):
+# Function to like any post of any user
+def post_a_like(instagram_username):
     media_id = get_post_id(instagram_username)
     request = (BASE_URL + "media/%s/likes") % (media_id)
     data = {"access_token": ACCESS_TOKEN}
@@ -140,43 +140,67 @@ def like_a_post(instagram_username):
         print "Your like was unsuccessful. Try again!"
 
 
-
+# Function to posy comment using username
 def post_a_comment(instagram_username):
     media_id = get_post_id(instagram_username)
-    request= (BASE_URL + "media/%s/comments") % (media_id)
-    data= {"access_token":ACCESS_TOKEN, "text":"thik hai"}
+    request = (BASE_URL + "media/%s/comments") % (media_id)
+    comment = raw_input("Comment?:")
+    data = {"access_token": ACCESS_TOKEN, "text": comment}
     print "POST request URL: %s" % (request)
-    comment_on_post=requests.post(request,data).json()
+    comment_on_post = requests.post(request, data).json()
     if comment_on_post["meta"]["code"] == 200:
         print "comment done"
     else:
         print"comment was unsuccessful"
 
 
-
+# Function to search the posts using hashtags
 def search_by_tag():
-    query= raw_input("Add a query:\n")
-    print "Get request Url:" + ((BASE_URL + "tags/%s/media/recent?access_token=%s")) % (query, ACCESS_TOKEN)
-    hashtag = requests.get(((BASE_URL + "tags/%s/media/recent?access_token=%s")) % (query, ACCESS_TOKEN)).json()
+    tag = raw_input("Search by tags. Enter any tag:\n")
+    print "Get request Url:" + ((BASE_URL + "tags/%s/media/recent?access_token=%s")) % (tag, ACCESS_TOKEN)
+    hashtag = requests.get(((BASE_URL + "tags/%s/media/recent?access_token=%s")) % (tag, ACCESS_TOKEN)).json()
     if hashtag["meta"]["code"] == 200:
         if len(hashtag["data"]):
-            for i in hashtag:
-                print hashtag["data"]["%d"]["id"] % (i)
-            return hashtag["data"]
+            return hashtag["data"][0]["id"]
         else:
             print "there is no such hashtag"
     else:
         print "Recieved status code is not 200"
 
 
+# Function to post any comment on any post of any user
+def post_a_comment_by_hashtag():
+    media_id = search_by_tag()
 
+    request = (BASE_URL + "media/%s/likes") % (media_id)
+    data = {"access_token": ACCESS_TOKEN}
+    print "POST request URL : %s" % (request)
+    post_a_like = requests.post(request, data).json()
+    if post_a_like['meta']['code'] == 200:
+        print "Liked!"
+    else:
+        print "Your like was unsuccessful. Try again!"
+
+
+    request_for_comment= (BASE_URL + "media/%s/comments") % (media_id)
+    comment = raw_input("comment?:")
+    data= {"access_token":ACCESS_TOKEN, "text":comment}
+    print "POST request URL: %s" % (request_for_comment)
+    comment_on_post=requests.post(request_for_comment,data).json()
+    if comment_on_post["meta"]["code"] == 200:
+        print "Comment done"
+    else:
+        print"Comment was unsuccessful"
+
+
+# Function to show the main menu of the instaBot
 def bot_main():
     while True:
         print '\n'"Hey! Welcome to instaBot!\n\nHere are your menu options:\n" \
-              "a.Get your own details\n\nb.Get details of a user by username\n\n" \
-              "c.Get your own recent post\n\nd.Get the recent post of a user by username\n\n" \
-              "e.Show the images liked by user\n\nf.Get the comments using media ID\n\ng.Like a post\n\n" \
-              "h.Comment on a post\ni.search by hashtag\n"
+              "a.Get your own details\nb.Get details of a user by username\n" \
+              "c.Get your own recent post\nd.Get the recent post of a user by username\n" \
+              "e.Show the images liked by user\nf.Get the comments using media ID\ng.Like a post\n" \
+              "h.Comment on a post\ni.search by hashtag\nj.Post a comment and like by searching hashtags"
         choose_option = raw_input("Choose any option from tha given menu:")
         if choose_option == "a":
             apni_jaankari()
@@ -195,12 +219,15 @@ def bot_main():
             get_the_comments(instagram_username)
         elif choose_option == "g":
             instagram_username = raw_input("Enter the username of user")
-            like_a_post(instagram_username)
+            post_a_like(instagram_username)
         elif choose_option == "h":
             instagram_username = raw_input("Enter the username of user")
             post_a_comment(instagram_username)
         elif choose_option == "i":
             search_by_tag()
+        elif choose_option == "j":
+            post_a_comment_by_hashtag()
+
 bot_main()
 
 
